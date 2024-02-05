a9404cc7629c5acdcd5eba4f1e98a5dbc6f0809a
ChernenkiyNikitych4732 <chernenkiy.nikitychsssr@gmail.com>
2024-02-05 15:33:02 +0100

    Курс 4 Домашнее заданее 46 Транзакции. Ретроспектива команды - изначальный комментарий

---
+++ src/test/java/skypro/javaunion/teamdevelopment/telegrambot/TelegramBotApplicationTests.java
@@ -0,0 +1,13 @@
+package skypro.javaunion.teamdevelopment.telegrambot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelegramBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
